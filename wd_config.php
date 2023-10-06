67865c46daec399f5c43a9fddf094d434795eff5
siddhi-123 <53900880+siddhi-123@users.noreply.github.com>
2023-10-06 19:01:55 +0530

    Add files via upload

---
+++ wd_config.php
@@ -0,0 +1,17 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','wdinternship3') or die('connection failed');
+
+?>
+
+<!-- $servername = 'localhost';  
+$username = '';  
+$password = '';  
+$database_name = 'abc';
+$conn = mysqli_connect($servername, $username, $password, $database_name);  
+if(! $conn )  
+{  
+  die('Could not connect: ' . mysqli_error());  
+}  
+echo 'Connected successfully';  
+mysqli_close($conn);  -->
